9981642988e779dbc0f7aadd4abc3c6d39dc1324
Head-Shot-53 <denchikmashtack@gmail.com>
2025-09-13 09:21:11 +0200

    chore: configure Celery with Redis

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,37 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Celery
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL",
+    "redis://localhost:6379/0",
+)
+
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "redis://localhost:6379/1",
+)
+
+CELERY_ACCEPT_CONTENT = [
+    "json",
+]
+
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+
+CELERY_TIMEZONE = "UTC"
+
+CELERY_RESULT_EXPIRES = 3600
+
+
+CELERY_BEAT_SCHEDULE = {
+    "expire-stale-reservations-every-minute": {
+        "task": (
+            "apps.inventory.tasks."
+            "expire_stale_reservations"
+        ),
+        "schedule": 60.0,
+    },
+}
